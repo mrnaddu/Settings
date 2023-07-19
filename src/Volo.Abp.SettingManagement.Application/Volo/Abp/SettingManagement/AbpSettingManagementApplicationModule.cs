2cd012c94bffaefce5fe0bf978e6ad7a6fe75eb8
nadeem <76742221+mrnaddu@users.noreply.github.com>
2023-07-19 17:35:36 +0530

    yan kaluve

---
+++ src/Volo.Abp.SettingManagement.Application/Volo/Abp/SettingManagement/AbpSettingManagementApplicationModule.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Application;
+using Volo.Abp.Emailing;
+using Volo.Abp.Modularity;
+using Volo.Abp.Timing;
+
+namespace Volo.Abp.SettingManagement;
+
+[DependsOn(
+    typeof(AbpDddApplicationModule),
+    typeof(AbpSettingManagementDomainModule),
+    typeof(AbpSettingManagementApplicationContractsModule),
+    typeof(AbpEmailingModule),
+    typeof(AbpTimingModule)
+)]
+public class AbpSettingManagementApplicationModule : AbpModule
+{
+}
